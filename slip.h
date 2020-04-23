9fffaf007c1ad5c4024d94c9ae96b6ed1ad52ef0
iharyadi <iharyadi@gmail.com>
2020-04-23 15:19:01 -0700

    Modify Slip to handle hardware and software serial

--- slip.h
+++ slip.h
@@ -10,8 +10,6 @@
 
 typedef unsigned char uchar;
 
-//#include <stdlib.h>
-
 #include <SoftwareSerial.h>
 
 /* SLIP special character codes
@@ -23,28 +21,104 @@ typedef unsigned char uchar;
 
 #define SLIP_MAXINBUFFERSIZE      255
 
+template<class TSer>
 class Slip
 {
 public:
     typedef void (*ReadCallback)(uint8_t *,uint8_t);
     
-    Slip(SoftwareSerial& serial);
-    ~Slip();
-    void proc();
-    bool sendpacket(uint8_t *buf,uint8_t len);
+    Slip(TSer& serial):_serial(serial){};
+    ~Slip(){}
+    void proc()
+    {
+        while (_serial.available() > 0)
+        {
+            handleByte(_serial.read());
+        }
+    };
+
+    bool sendpacket(uint8_t *buf,uint8_t len)
+    {
+        _serial.write(SLIP_END);
+        for(int i=0;i<len;i++)
+        {
+            switch(buf[i])
+            {
+            case SLIP_END:
+                _serial.write(SLIP_ESC);
+                _serial.write(SLIP_ESC_END);
+                break;
+            case SLIP_ESC:
+                _serial.write(SLIP_ESC);
+                _serial.write(SLIP_ESC_ESC);
+                break;
+            default:
+                _serial.write(buf[i]);
+            }
+        }
+        _serial.write(SLIP_END);
+        return true;
+    };
+
     void setCallback(ReadCallback cb)
     {
         readCallback = cb;
-    }
+    };
+
 private:
+    void handleByte(uint8_t c)
+    {
+
+        if(c==SLIP_END)
+        {
+            if(inbuffer_ndx>0)
+            {
+                if(readCallback)
+                {
+                    readCallback(inbuffer,inbuffer_ndx);
+                }
+            }
+            inbuffer_ndx=0;
+            lastbyte=c;
+            return;
+        }
+
+        if(inbuffer_ndx>=SLIP_MAXINBUFFERSIZE)inbuffer_ndx=0;
+
+        if(lastbyte==SLIP_ESC)
+        {
+            switch(c)
+            {
+            case SLIP_ESC_END:
+                inbuffer[inbuffer_ndx]=SLIP_END;
+                inbuffer_ndx++;
+                break;
+            case SLIP_ESC_ESC:
+                inbuffer[inbuffer_ndx]=SLIP_ESC;
+                inbuffer_ndx++;
+                break;
+            }
+            lastbyte=c;
+            return;
+        }
+
+        if(c!=SLIP_ESC)
+        {
+            inbuffer[inbuffer_ndx]=c;
+            inbuffer_ndx++;
+        }
+
+        lastbyte=c;
+    };
 
     ReadCallback readCallback = NULL;
     uint8_t lastbyte = 0;
-    void handleByte(uint8_t c);
     uint8_t inbuffer[SLIP_MAXINBUFFERSIZE];
     uint8_t inbuffer_ndx=0;
-    SoftwareSerial& _serial;
+    TSer& _serial;
 };
 
+typedef Slip<SoftwareSerial> SoftwareSlip;
+typedef Slip<HardwareSerial> HardwareSlip;
 
 #endif
